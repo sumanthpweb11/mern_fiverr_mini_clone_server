c9b71c1ec3cf1a3122e6450f044c6ea8c1196654
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-03-11 22:40:40 +0530

    c

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ app.use(
     credentials: true,
   })
 );
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-app.use(cookieParser());
+
 dotenv.config();
 mongoose.set("strictQuery", true);
 
